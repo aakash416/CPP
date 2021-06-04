6ecde5785156e9fd277fbaaf6c8e6be812f04eb0
Aakash Raj <aakashraj416@gmail.com>
2021-06-04 18:45:07 +0530

    Add files via upload

---
+++ ROMAN_NUMERALS.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************
+ROMAN NUMERALS
+
+Write a program to convert a given string representing an integer in Roman Numeral Format
+
+to an integer in Decimal Numeral Format.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int value(char r)
+{
+    if (r == 'I')
+        return 1;
+    if (r == 'V')
+        return 5;
+    if (r == 'X')
+        return 10;
+    if (r == 'L')
+        return 50;
+    if (r == 'C')
+        return 100;
+    if (r == 'D')
+        return 500;
+    if (r == 'M')
+        return 1000;
+
+    return -1;
+}
+int romanToDecimal(string &str)
+{
+    int res = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        int s1 = value(str[i]);
+        if (i + 1 < str.length())
+        {
+            int s2 = value(str[i + 1]);
+
+            if (s1 >= s2)
+            {
+                res = res + s1;
+            }
+            else
+            {
+                res = res + s2 - s1;
+                i++;
+            }
+        }
+        else
+        {
+            res = res + s1;
+        }
+    }
+    return res;
+}
+int main()
+{
+    string str;
+    cin >> str;
+    cout << romanToDecimal(str);
+    return 0;
+}
